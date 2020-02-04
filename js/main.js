edb45825bc5cca641d07349df5014187c8ced6b7
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2020-02-04 15:43:02 -0600

    Implementación cifrado Cesar

---
+++ js/main.js
@@ -0,0 +1,105 @@
+            
+let algoritmo;
+let accion;
+let labelClave;
+let clave;
+let editor;
+let resultado;
+let placheolder;
+
+/**
+ * @description pausa la ejecución de una función para evitar que se ejecute demasiadas veces.
+ * @param {Function} fuc Es la función que se ejecutara al terminar el timer.
+ * @param {number} delay Es el tiempo de espera despues de cada ejecución de esta función.
+ * @returns {Function} Regresa una función debounce.
+ * @author stackoverflow
+ */
+function debounce(fuc,delay){
+    let debounceTimer;
+    return function(){
+        const context=this;
+        const args=arguments;
+        clearTimeout(debounceTimer);
+        debounceTimer=setTimeout(()=>fuc.apply(context,args),delay);
+
+    }
+}
+
+
+/**
+ * @description Función que configura el arranque de la aplicación.
+ * @author Brenda Barrios Becerra
+ */
+function main(){
+    algoritmo=document.querySelector('#algoritmo');
+    accion=document.querySelector("#accion");
+    labelClave=document.querySelector("#label-clave");
+    clave=document.querySelector("#input-clave");
+    editor=document.querySelector("#editor");
+    resultado=document.querySelector("#resultado");
+    placheolder=document.createElement('p');
+    placheolder.id='placeholder';
+    resultado.append(placheolder)
+    let listener=debounce(function(e){
+        checkInputs();
+    },1000);
+    algoritmo.addEventListener('change',listener,false);
+    accion.addEventListener('change',listener,false);
+    clave.addEventListener('input',listener,false);
+    editor.addEventListener('keyup',listener,false);
+}
+
+
+
+function checkInputs(){
+    console.log(editor);
+    let valorTexto=editor.value
+    let tipoAlgoritmo=algoritmo.selectedIndex
+    let cifrarDesifrar=accion.selectedIndex
+    let ingresaClave=clave.value
+    placheolder.innerHTML= `Texto: ${valorTexto}<br>
+    Algoritmo: ${tipoAlgoritmo}<br>
+    Acción:${cifrarDesifrar}<br>
+    Clave: ${ingresaClave}<br>`;
+
+
+    if ([2,3,4,6].includes(tipoAlgoritmo)) {
+        labelClave.style.visibility = 'visible';
+        clave.style.visibility = 'visible';
+        
+    } else {
+        labelClave.style.visibility = 'hidden';
+        clave.style.visibility = 'hidden';
+        
+    }
+
+
+    switch (tipoAlgoritmo) {
+        case 0:
+            placheolder.innerText = cesar(valorTexto, accion.value);
+            break;
+
+        case 1:
+            
+            break;
+
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;  
+        case 4:
+            
+            break;
+        case 5:
+            
+            break;
+        case 6:
+            
+            break;
+        
+        default:
+            break;
+    }
+}
---
+++ js/polybios.js
@@ -0,0 +1,20 @@
+
+    function polybios(texto){
+        let mensajecifrado=" "
+        let arreglo=[
+          ['a','b','c','d','e'],
+          ['f','g','h','i/j','k'],
+          ['l','m','n','o','p'],
+          ['q','r','s','t','u'],
+          ['v','w','x','y','z']
+
+        ]
+        for (let letra of texto){
+            for (let columna of arreglo){
+                for(let fila of columna){
+
+                }
+
+            }
+        }
+    }
